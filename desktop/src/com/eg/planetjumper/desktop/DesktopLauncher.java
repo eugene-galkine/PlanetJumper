36a2da86d66350c613e2aabd4121cfb8f0ddd552
Eugene <eugene.galkine@gmail.com>
2017-08-15 18:27:28 -0500

    initial commit

---
+++ desktop/src/com/eg/planetjumper/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.eg.planetjumper.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.eg.planetjumper.PlanetJumper;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PlanetJumper(), config);
+	}
+}
